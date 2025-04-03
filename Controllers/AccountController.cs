ca42e3e3d89cbadf8f0ca823f9615aa98db214ff
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2025-04-03 11:47:55 -0400

    Update Implementated

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,26 +6,40 @@ using Services.UserServices;
 
 namespace CloudShield.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AccountController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class AccountController : ControllerBase
+  {
+    private readonly IUserCommandCreate _user;
+    private readonly IUserCommandsUpdate _userUpdate;
+
+    public AccountController(IUserCommandCreate user, IUserCommandsUpdate userUpdate)
     {
-        private readonly  IUserCommandCreate _user;
-
-        public AccountController(IUserCommandCreate user)
-        {
-            _user = user;
-        }
-
-          [HttpPost("Add")]  
-          public async Task<IActionResult> Add([FromBody] UserDTO user)
-          {
-                    ApiResponse<bool> result = await _user.AddNew(user);
-
-                    if(result.Success==false) return BadRequest(new {result});
-
-                    return Created($"api/users/{user.Id}",result);
-          }
-
+      _user = user;
+      _userUpdate = userUpdate;
     }
+
+    [HttpPost("Add")]
+    public async Task<IActionResult> Add([FromBody] UserDTO user)
+    {
+      ApiResponse<bool> result = await _user.AddNew(user);
+
+      if (result.Success == false) return BadRequest(new { result });
+
+      return Created($"api/users/{user.Id}", result);
+    }
+
+    [HttpPut("Update")]
+    public async Task<IActionResult> Update([FromBody] UserDTO userDto)
+    {
+      var result = await _userUpdate.Update(userDto);
+
+      if(!result.Success)
+      {
+        return BadRequest(new {result});
+      }
+
+      return Ok(new { result });
+    }
+  }
 }
